faece98cd12cb2eb2d456207a7007179ccaad6c4
cnhtatham <cnhtatham@gmail.com>
2018-03-06 10:49:13 +0000

    animations

---
+++ script.js
@@ -0,0 +1,8 @@
+function animateTriangles() {
+
+}
+
+document.getElementById("mainButton").addEventListener("click", function() {
+    $(".rightTriangle").addClass("fadeOutRight")
+    $(".leftTriangle").addClass("fadeOutLeft")
+})
