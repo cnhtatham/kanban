e682a050e06562a6e67a390ac47d356e74ba83e3
Harry Tomlinson <harry.t.123@hotmail.com>
2018-03-06 13:53:09 +0000

    Sidebar complete

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-document.getElementById("mainButton").addEventListener("click", function () {
-    $(".rightTriangle").animate({
-        width: "0px"
-    }, 700)
-    $(".leftTriangle").animate({
-        width: "0px"
-    }, 700)
-    $("#mainButton").animate({
-        top: "90%",
-        opacity: "0"
-    }, 700)
-    $(".mainLogo").animate({
-        top: "30%",
-        opacity: "0"
-    }, 700)
-    setTimeout(function () {
-        $("#mainButton").css("display", "none")
-        $(".rightTriangle").css("display", "none")
-        $(".leftTriangle").css("display", "none")
-        $(".mainLogo").css("display", "none")
+$(document).ready(function () {
 
-        $("#imran").animate({
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+    document.getElementById("mainButton").addEventListener("click", function () {
+        $(".rightTriangle").animate({
+            width: "0px"
+        }, 700)
+        $(".leftTriangle").animate({
+            width: "0px"
+        }, 700)
+        $("#mainButton").animate({
+            top: "90%",
             opacity: "0"
-        }, 1000)
-        $("#stuart").animate({
+        }, 700)
+        $(".mainLogo").animate({
+            top: "30%",
             opacity: "0"
-        }, 1000)
-        setTimeout(function() {
-            $("#imran").css("display", "none")
-            $("#stuart").css("display", "none")
-        }, 1000)
-    }, 700)
-})
+        }, 700)
+        setTimeout(function () {
+            $("#mainButton").css("display", "none")
+            $(".rightTriangle").css("display", "none")
+            $(".leftTriangle").css("display", "none")
+            $(".mainLogo").css("display", "none")
+
+            $("#imran").animate({
+                opacity: "0"
+            }, 1000)
+            $("#stuart").animate({
+                opacity: "0"
+            }, 1000)
+            setTimeout(function () {
+                $("#imran").css("display", "none")
+                $("#stuart").css("display", "none")
+            }, 1000)
+        }, 700)
+    })
+});
