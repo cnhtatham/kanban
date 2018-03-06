6b9865971db1de5ad24391df00f0aeb2879938c4
cnhtatham <cnhtatham@gmail.com>
2018-03-06 11:29:32 +0000

    updated animations

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ function animateTriangles() {
 
 }
 
-document.getElementById("mainButton").addEventListener("click", function() {
-    $(".rightTriangle").addClass("fadeOutRight")
-    $(".leftTriangle").addClass("fadeOutLeft")
+document.getElementById("mainButton").addEventListener("click", function () {
+    $(".rightTriangle").animate({
+        width: "0px"
+    }, 700)
+    $(".leftTriangle").animate({
+        width: "0px"
+    }, 700)
+
+    $("#mainButton").animate({
+        top: "100%",
+        opacity: "0"
+    }, 700)
+    setTimeout(function () {
+        $("#mainButton").css("display", "none")
+        $(".rightTriangle").css("display", "none")
+        $(".leftTriangle").css("display", "none")
+    }, 568)
 })
